36d14dffb83d1516ee52402bd7358d5b8eb8a80a
Jay Thakkar <jay.thakkar@mindinventory.com>
2022-05-23 11:37:48 +0530

    initial file structure

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`)
+    console.log(`db port - ${process.env.DB_HOST}`)
+})
